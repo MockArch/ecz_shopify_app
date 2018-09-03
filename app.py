44c918dd742e8ee5d41a8e7ecb043f8cfcec5b81
Mock <arch.gp@yandex.com>
2018-09-03 17:43:58 +0530

    new updated

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask
-from datetime import datetime
-app = Flask(__name__)
+import os
+from flask import Flask, request
+from pprint import pprint
 
+
+app = Flask(__name__)
+API_KEY = os.getenv("API_KEY")
+SECRET_KEY =  os.getenv("SECRET_KEY")
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
     <h1>Hello Shopify app</h1>"""
+
+
+@app.route('/shopify')
+def shopify_page():
+    pprint(request)
+    return "something is requested"
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
