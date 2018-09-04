69a59d277d35478114d39e0a3a940b7f0a47bfd4
Mock <arch.gp@yandex.com>
2018-09-04 09:55:43 +0530

    update 3...

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for, Response
 from pprint import pprint
 
 
 app = Flask(__name__)
 API_KEY = os.getenv("API_KEY")
 SECRET_KEY = os.getenv("SECRET_KEY")
-
+URL = "https://shopifyec.herokuapp.com/"
+ACCESS_SCOPE = 'read_orders, write_products'
 
 @app.route('/')
 def homepage():
@@ -16,11 +17,23 @@ def homepage():
 
 
 @app.route('/shopify')
-def shopify_page():
-    hmac = request.args.get("hmac")
-    shop = request.args.get("shop")
-    r = "the hmac return value is " + hmac + "and " + " the shop name is " + shop
-    return r
+def shopify_install():
+    # hmac = request.args.get("hmac")
+    if request.args.get("shop"):
+        shop = request.args.get("shop")
+    else:
+        return Response(response="Error: parameter shop not found ", status=500)
+    r_url = URL + '/shopify/callback'    
+    auth_url = 'https://{0}/admin/oauth/authorize?client_id={1}&scope={2}&redirect_uri={3}'.format(
+        shop, API_KEY , ACCESS_SCOPE , r_url)
+    # r = "the hmac return value is " + hmac + "and " + " the shop name is " + shop
+    print("Debug - auth URL: ", auth_url)
+    return redirect(auth_url)
+
+
+@app.route('/shopify/callback')
+def shopify_callback():
+    return "REDIRECT URL"
 
 
 if __name__ == '__main__':
